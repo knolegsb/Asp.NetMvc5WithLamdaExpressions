e391908393a6437a0811c7f8e80034a7e969debc
knolegsb <seon2you@hotmail.com>
2015-02-17 23:10:07 -0800

    lamda expression

---
+++ Asp.NetMvc5WithLambdaExpressions/Context/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Asp.NetMvc5WithLambdaExpressions.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetMvc5WithLambdaExpressions.Context
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Asp.NetMvc5WithLambdaExpressions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Asp.NetMvc5WithLambdaExpressions.Startup))]
+namespace Asp.NetMvc5WithLambdaExpressions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
